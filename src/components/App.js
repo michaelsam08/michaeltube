ef643b1e8d68c974e7e17f9b0eb808192c676e0e
MichaelSam <michaelsam08@gmail.com>
2019-06-10 17:35:05 -0500

    video detail

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Searchbar from "./Searchbar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
@@ -14,11 +16,20 @@ class App extends React.Component {
     // response.data.items;
     this.setState({ videos: response.data.items });
   };
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <Searchbar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          //prop passed down and cb function named the same
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
